b1b704ba8b0f059da288f6ec4f2379ed10d35e7c
ptsne1002 <ptsne1002@gmail.com>
2022-12-09 01:16:17 +0700

    create function random custome food and change show message

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 import asyncio
 import random
+import json
 
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
@@ -9,35 +10,63 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.all()
-client = discord.Client(command_prefix='!', intents=intents)
+client = discord.Client(command_prefix='bcs!', intents=intents)
 
-list_food = ["Gà", "Vịt quay bánh mì", "Bánh tráng trộn", "Bánh mì", "Bò Bía", "Pizza", "Bánh Bao"]
+#data
+list_food = {
+  "Gà nướng lu": "https://shopeefood.vn/ho-chi-minh/ga-nuong-lu-viet-huong-le-van-tho?shareChannel=copy_link",
+  "Vịt quay bánh mì": "Quán đầu đường Nguyễn Văn Quá",
+  "Bánh tráng trộn": "url",
+  "Bánh mì": "https://shopeefood.vn/ho-chi-minh/banh-mi-dong-ri",
+  "Bò Bía": "url",
+  "Pizza": "url",
+  "Bánh Bao nướng": "https://shopeefood.vn/ho-chi-minh/ga-nuong-o-o-o-ga-nuong-nhieu-vi-nguyen-hong-dao?shareChannel=copy_link"
+}
+
 list_drink = ["Trà Sữa", "Nước Ép", "Highlands"]
+
+leader = "<@801662918600163379>"
+
+#handle
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    auto_printer.start()
+    # auto_printer.start()
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-        
-    if message.content.startswith('!random food'):
-        result = random.choice(list_food)
-        await message.channel.send(f"@everyone Đồ ăn random được hôm nay là: {result}")
-
-    if message.content.startswith('!list food'):
-        await message.channel.send(f"@everyone list food: {list_food}")
     
-    if message.content.startswith('!random drink'):
+    if message.content.startswith('bcs! list food'):
+        foods = list(list_food.keys())
+        await message.channel.send(f"@everyone list food nè: {foods}.")
+        
+    if message.content.startswith('bcs! random food'):
+        food_key = list(list_food.keys())
+        food = random.choice(food_key)
+        await message.channel.send(f"@everyone Đồ ăn random được hôm nay là: **{food}** ({list_food[food]}).\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        await message.channel.send(f"")
+
+    if message.content.startswith('bcs! custom list food :'):
+        str = message.content
+        food_custom_raw = str.split(":")[1]
+        food_custom = food_custom_raw.split(",")
+        rs = random.choice(food_custom)
+        await message.channel.send(f"kết quả random được là: **{rs}**.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+
+    
+    if message.content.startswith('bcs! list drink'):
+        await message.channel.send(f"@everyone list drink nè: {list_drink}.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        
+    if message.content.startswith('bcs! random drink'):
         result = random.choice(list_drink)
-        await message.channel.send(f"@everyone Đồ uống random được hôm nay là: {result}")
+        await message.channel.send(f"@everyone Đồ uống random được hôm nay là: {result}.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        
 
-    if message.content.startswith('!list drink'):
-        await message.channel.send(f"@everyone list drink: {list_drink}")
-
+#auto
 @tasks.loop(seconds=5)
 async def auto_printer():
     message_channel = client.get_channel(1050101954140983446)
