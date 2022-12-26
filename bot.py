a8b7fa7c933005511d9250588747cedd1d7d4f55
ptsne1002 <ptsne1002@gmail.com>
2022-12-26 23:43:30 +0700

    create function read data from file

--- bot.py
+++ bot.py
@@ -15,26 +15,24 @@ client = discord.Client(command_prefix='bcs!', intents=intents)
 
 
 #data
-list_food = {
-  "Gà nướng lu": "https://shopeefood.vn/ho-chi-minh/ga-nuong-lu-viet-huong-le-van-tho?shareChannel=copy_link",
-  "Vịt quay bánh mì": "Quán đầu đường Nguyễn Văn Quá",
-  "Bánh tráng trộn": "url",
-  "Bánh mì": "https://shopeefood.vn/ho-chi-minh/banh-mi-dong-ri",
-  "Bò Bía": "url",
-  "Pizza": "url",
-  "Bánh Bao nướng": "https://shopeefood.vn/ho-chi-minh/ga-nuong-o-o-o-ga-nuong-nhieu-vi-nguyen-hong-dao?shareChannel=copy_link"
-}
+with open("data.json", "r", encoding='utf-8') as f:
+    data = json.loads(f.read())
 
-list_drink = ["Trà Sữa", "Nước Ép", "Highlands"]
+list_food = data.get('list food')
+list_drink = data.get('list drink')
+
 
 leader = "<@801662918600163379>"
-
-
+hg_id = "<@853495845239390208>"
+ttoan_id = "<@886786917931302932>"
+dthien_id = "<@868753788608069663>"
 #handle
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    
+    # message_channel = client.get_channel(844487320081924136)
+    # await message_channel.send(f"@everyone Bot mới nghe được tin mấy bé CTU {hg_id}, {ttoan_id}, {dthien_id} báo cáo luận văn thành công mỹ mãn nên sẽ đãi mọi người nguyên vườn gà ấy... :stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes:")
+
 
 
 @client.event
@@ -42,6 +40,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     
+    #Food
     if message.content.startswith('bcs! list food'):
         foods = list(list_food.keys())
         await message.channel.send(f"@everyone list food nè: {foods}.")
@@ -49,22 +48,24 @@ async def on_message(message):
     if message.content.startswith('bcs! random food'):
         food_key = list(list_food.keys())
         food = random.choice(food_key)
-        await message.channel.send(f"@everyone Đồ ăn random được hôm nay là: **{food}** ({list_food[food]}).\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        await message.channel.send(f"@everyone Đồ ăn random được hôm nay là: **{food}** \n{list_food[food]} .\nĂn uống nhiệt tình đi Sếp {leader} lo hết.")
 
     if message.content.startswith('bcs! custom list food :'):
         str = message.content
         food_custom_raw = str.split(":")[1]
         food_custom = food_custom_raw.split(",")
         rs = random.choice(food_custom)
-        await message.channel.send(f"kết quả random được là: **{rs}**.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        await message.channel.send(f"kết quả random được là: **{rs}**.\nĂn uống nhiệt tình đi Sếp {leader} lo hết.")
 
-    
+    #Drink
     if message.content.startswith('bcs! list drink'):
-        await message.channel.send(f"@everyone list drink nè: {list_drink}.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        await message.channel.send(f"@everyone list drink nè: {list_drink}.\nĂn uống nhiệt tình đi Sếp {leader} lo hết.")
         
     if message.content.startswith('bcs! random drink'):
-        result = random.choice(list_drink)
-        await message.channel.send(f"@everyone Đồ uống random được hôm nay là: {result}.\nĂn uống nhiệt tình đi xếp {leader} lo hết.")
+        drink_key = list(list_drink.keys())
+        drink = random.choice(drink_key)
+        await message.channel.send(f"@everyone Đồ uống random được hôm nay là: **{drink}** \n{list_drink[drink]} .\nĂn uống nhiệt tình đi Sếp {leader} lo hết.")
+    
     
     if message.content.startswith('bcs! get voucher'):
         vouchers = get_voucher()
@@ -72,7 +73,16 @@ async def on_message(message):
         for key, value in vouchers.items():
             str += f"**{key}** : {value}" + "\n"
         await message.channel.send(str)
-        
+    
+    if message.content.startswith('bcs! note'):
+        await message.channel.send(f"@everyone Bot mới nghe được tin là {hg_id} và {ttoan_id} tuần này sẽ remote nên tranh thủ ăn lẹ lẹ mọi người ơi. :stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes:") 
+    
+    if message.content.startswith('bcs! notification'):
+        await message.channel.send(f"@everyone Bot mới nghe được tin mấy bé CTU {hg_id}, {ttoan_id}, {dthien_id} báo cáo luận văn thành công mỹ mãn nên sẽ đãi mọi người nguyên vườn gà ấy... :stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes:")
+    
+    if message.content.startswith('bcs! bot') or message.content.startswith('bcs! oc'):
+        await message.channel.send(f"Chửi bot, bot đấm cho không cản được đâu nha.. :rolling_eyes::rolling_eyes::rolling_eyes:")   
+    
     if message.content.startswith('bcs! set time'):
         auto_printer.start()
 
